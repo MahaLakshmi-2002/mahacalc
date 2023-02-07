f9b788543cc072e3cea7c22857655620ad069c1c
DIGSAN <DIGSAN@10.4.194.152>
2023-02-07 16:21:14 +0530

    committtt

---
+++ maven2/src/main/java/calc/Calc.java
@@ -0,0 +1,18 @@
+package calc;
+
+public class Calc {
+	int a,b;
+	public int addition(int a, int b) {
+		return a+b;
+	}
+	public int subtract(int a, int b) {
+		return a-b;
+	}
+	public int multiplication(int a, int b) {
+		return a*b;
+	}
+	public int division(int a, int b) {
+		return a/b;
+	}
+
+}
